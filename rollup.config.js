a00109ca0bf9f1126f325073f29d1008ad1f49f0
刘松(宙斯) <zeus.ls@alibaba-inc.com>
2020-05-07 15:43:13 +0800

    minor changes

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,12 @@
 import dts from 'rollup-plugin-dts';
 import ts from '@rollup/plugin-typescript';
 
-const typescript = ({ input, output, ...options }) => {
+const typescript = ({ input, output, types, ...options }) => {
   return [
     {
       input,
       output: {
-        file: `./dist/${output}.js`,
+        file: `${output}.js`,
         ...options,
       },
       plugins: [ts()]
@@ -14,7 +14,7 @@ const typescript = ({ input, output, ...options }) => {
     {
       input,
       output: {
-        file: `./types/${output}.d.ts`
+        file: types || `${output}.d.ts`
       },
       plugins: [dts()]
     }
@@ -24,34 +24,35 @@ const typescript = ({ input, output, ...options }) => {
 
 export default [].concat(
   typescript({
+    format: 'umd',
     name: 'Flyover',
     input: 'src/index.ts',
-    output: 'flyover',
-    format: 'umd',
+    output: 'dist/flyover',
+    types: './types/index.d.ts'
   }),
   typescript({
     input: 'src/alipay/index.ts',
-    output: 'flyover-alipay',
+    output: 'lib/alipay/index',
     format: 'cjs'
   }),
   typescript({
     input: 'src/mybank/index.ts',
-    output: 'flyover-mybank',
+    output: 'lib/mybank/index',
     format: 'cjs'
   }),
   typescript({
     input: 'src/koubei/index.ts',
-    output: 'flyover-koubei',
+    output: 'lib/koubei/index',
     format: 'cjs'
   }),
   typescript({
     input: 'src/qianniu/index.ts',
-    output: 'flyover-qianniu',
+    output: 'lib/qianniu/index',
     format: 'cjs'
   }),
   typescript({
     input: 'src/taobao/index.ts',
-    output: 'flyover-taobao',
+    output: 'lib/taobao/index',
     format: 'cjs'
   }),
 );
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,2 +1,11 @@
-export * from './flyover';
-export * from './flyover-alipay';
+declare const call: (method: any, ...params: any[]) => any;
+declare const toast: (content: any, options?: any) => any;
+declare const alert: (content: any, options?: any) => any;
+declare const confirm: (content: any, options?: any) => any;
+declare const showLoading: () => any;
+declare const hideLoading: () => any;
+declare const pushWindow: (url: any, options?: any) => any;
+declare const popWindow: () => any;
+declare const closeWebview: () => any;
+
+export { alert, call, closeWebview, confirm, hideLoading, popWindow, pushWindow, showLoading, toast };
